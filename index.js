4899b6e0788d872c5b8ac42f49b5381f63f01fc2
riquelmer45 <riquelmer_modesto@hotmail.com>
2022-10-23 14:43:04 -0300

    Revert "Implementação do cadastro de funcionarios e visualização desses dados"
    This reverts commit 076542e2218e36b356e9061c44de8a4865d1dc4b.

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const exphbs = require('express-handlebars')
 const petshop = require('./routes/router')
 const Pet = require('./routes/petRouter')
 const Cliente = require('./routes/clienteRouter')
-const Funcionario = require('./routes/funcionarioRouter')
 const conn = require('./db/conn')
 const petModel = require('./model/Pets')
 const clienteModel = require('./model/Cliente')
-const funcionarioRouter = require('./model/Funcionario')
 
 const hbs = exphbs.create({
   partialsDir: ["views/partials"]
@@ -35,8 +33,6 @@ app.use('/petshop', petshop )
 app.use('/pets', Pet)
 //adicionando rota Cliente
 app.use('/cliente', Cliente)
-//adcionando a rota Funcionario
-app.use('/funcionarios', Funcionario)
 
 app.get('/', (req, res) => {
   res.render('petshop/home')
